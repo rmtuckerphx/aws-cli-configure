15e74bf0e2a506055b8b828e0db1fd660659e14e
Mark Tucker <mark@shazaml.com>
2018-06-08 22:25:30 -0700

    Add aws-cli.list-profiles.credentials and statusbar entry

--- extension.js
+++ extension.js
@@ -5,7 +5,10 @@ const path = require('path');
 const os = require('os');
 const opn = require('opn');
 const fs = require('fs');
+const profileHandler = require('aws-profile-handler');
+const hash = require('object-hash');
 
+const DEFAULT_PROFILE = 'default';
 
 function openCredentialsFile(previewFlag = true) {
 
@@ -43,6 +46,107 @@ function openOnlineDocs() {
 
 }
 
+function listProfilesCredentials() {
+    const profiles = getSortedProfilesCredentials(false);
+
+    let message = `Here are the profiles: ${profiles.join(', ')}`;
+
+    vscode.window.showInformationMessage(message);
+}
+
+function getSortedProfilesCredentials(includeDefaultProfile = true) {
+
+    const credentialsFile = path.join(os.homedir(), '.aws', 'credentials');
+    let result = [];
+
+    if (fs.existsSync(credentialsFile)) {
+
+        // listProfile will check if file is valid. If it's not valid,
+        // no need to proceed, just callback the error.
+        try {
+            // if the credential file is empty, CLI will name the first profile as 'default'
+            const profiles = profileHandler.listProfiles();
+
+            if (includeDefaultProfile === false) {
+                const index = profiles.indexOf(DEFAULT_PROFILE);
+
+                if (index > -1) {
+                    profiles.splice(index, 1);
+                }
+            }
+
+            result = profiles.sort();
+
+        } catch (error) {
+            vscode.window.showWarningMessage(`File '${credentialsFile}' is not valid.`);
+            console.log(error);
+        }
+
+    }
+    else {
+        vscode.window.showInformationMessage(`File '${credentialsFile}' does not exist.`);
+    }
+
+    return result;
+}
+
+function updateStatus(status) {
+    let text = getDefaultProfileSetTo();
+    if (text) {
+        status.text = 'AWS CLI: ' + text;
+    }
+
+    if (text) {
+        status.show();
+    } else {
+        status.hide();
+    }
+}
+
+function getDefaultProfileSetTo() {
+
+    // if there is no default profile or named profile, then show <none>
+    let text = '<none>';
+
+    const profiles = getSortedProfilesCredentials();
+
+    const hasDefault = profiles.includes(DEFAULT_PROFILE);
+
+    if (hasDefault) {
+        // if there is a default profile, show it 
+        text = DEFAULT_PROFILE;
+    }
+
+    if (hasDefault && profiles.length > 1) {
+
+        const defaultProfile = profileHandler.getProfileCredentials(DEFAULT_PROFILE);
+
+        if (defaultProfile) {
+
+            profiles.forEach(profile => {
+
+                if (profile !== DEFAULT_PROFILE) {
+                    const candidateProfile = profileHandler.getProfileCredentials(profile);
+
+                    if (hash(defaultProfile) === hash(candidateProfile)) {
+
+                        // if there is a named profile that has exactly the same
+                        // object keys and values (ie hash) as a named profile, show it
+                        text = profile;
+                    }
+                }
+            });
+
+        }
+
+
+    }
+
+
+    return text;
+
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -52,6 +156,22 @@ function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('aws-cli.open.config', openConfigFile));
     context.subscriptions.push(vscode.commands.registerCommand('aws-cli.open.both', openBothFiles));
     context.subscriptions.push(vscode.commands.registerCommand('aws-cli.browse.docs', openOnlineDocs));
+
+    context.subscriptions.push(vscode.commands.registerCommand('aws-cli.list-profiles.credentials', listProfilesCredentials));
+
+    const status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    // status.command = 'extension.selectedLines';
+    context.subscriptions.push(status);
+
+    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((doc) => {
+        const credentialsFile = path.join(os.homedir(), '.aws', 'credentials').toLowerCase();
+
+        if (doc.fileName.toLowerCase() === credentialsFile) {
+            updateStatus(status);
+        }
+    }));
+
+    updateStatus(status);
 
 };
 
